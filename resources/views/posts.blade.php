546074180530cf78dfb65d20335fc4a534ff8db2
Charles Eduardo <charleseduardofaria@gmail.com>
2021-12-03 10:52:15 -0300

    layouts e migration

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -14,10 +14,7 @@
                 <div class="card-body">
                     <h5 class="card-title">Special title treatment</h5>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="detail-post.html" class="btn btn-primary">Go somewhere</a>
-                </div>
-                <div class="card-footer text-muted">
-                    2 days ago
+                    <a href="detail-post.html" class="btn btn-primary">Ler matéria</a>
                 </div>
             </div>
         </div>
