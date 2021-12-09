d0ad3a159d38eb00fabef945c42b61636f2e77a8
Charles Eduardo <charleseduardofaria@gmail.com>
2021-12-09 20:32:51 -0300

    removed unused line

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,7 +5,6 @@
     <title>@yield('title')</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
     <!--===============================================================================================-->
     <link rel="icon" type="/image/png" href="/images/icons/icon-discoteca.png" />
     <script src="https://use.fontawesome.com/releases/v5.15.4/js/all.js" crossorigin="anonymous"></script>
